2c7abb7f5028e4ac0611d99ea799d878d917bc0c
moshaid <alabidunmoshood@gmail.com>
2024-09-09 15:15:53 +0100

    update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -51,7 +51,7 @@ class Home extends Component {
                         <span>The New Way to Buy and Sell Homes <br></br> Commission-free.</span>
 
                         <div className="prop">
-                            <a className="project" href="https://moshaid.github.io/Richr/" target="_blank">VIEW PROJECT</a>
+                            <a className="project" href="https://moshaid.github.io/Richr/" target="_blan">VIEW PROJECT</a>
                         </div>
                     </div>                  
                 </div>
@@ -71,7 +71,7 @@ class Home extends Component {
                         {/* <span>FitHUB is a fitness platfrom that  <br></br> provides streamlined communication  <br></br> an online and onsite focused workout  <br></br> plan including remote check-ins.</span> */}
 
                         <div className="prop">
-                            <a className="project" href="">VIEW PROJECT</a>
+                            <a className="project" href="# al">VIEW PROJECT</a>
                         </div>
                     </div>                  
                 </div>
